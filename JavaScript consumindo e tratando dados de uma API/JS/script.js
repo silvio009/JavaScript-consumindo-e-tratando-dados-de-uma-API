06c9bf9b02ea8f4a48461c126c6464c5f480c080
silvio009 <rm550821@fiap.com.br>
2023-06-20 16:11:01 -0300

    then / catch / finally

--- JavaScript consumindo e tratando dados de uma API/JS/script.js
+++ JavaScript consumindo e tratando dados de uma API/JS/script.js
@@ -1,2 +1,14 @@
  // sincrono -> tarefa é concluída uma após outra
  // assímcrono-> tarefas executadas em "segundo plano"
+const consultaCEP = fetch ('https://viacep.com.br/ws/06755300/json/') // fatch recebe a URL da API 
+    .then(resposta => resposta.json())  
+    .then(r => {
+        if(r.erro){
+          throw Error('Esse CEP não existe!') 
+        }else
+        console.log(r)
+    })
+    .catch(erro => console.log(erro))  // catch sinaliza o erro 
+    .finally(mensagem=> console.log('processo concluído'))
+    
+console.log(consultaCEP)
