e90a653a18a5c3643e1887b6a212c7308a8f5219
silvio009 <rm550821@fiap.com.br>
2023-06-21 16:42:30 -0300

    terminado o projeto!

--- JavaScript consumindo e tratando dados de uma API/JS/script.js
+++ JavaScript consumindo e tratando dados de uma API/JS/script.js
@@ -1,16 +1,35 @@
  // sincrono -> tarefa é concluída uma após outra
  // assímcrono-> tarefas executadas em "segundo plano"
-async function buscarEndereco(CEP){
+async function buscarEndereco(cep){
+  const mensagemErro = document.getElementById('erro');
+  mensagemErro.innerHTML = "";
     try{
-      const consultaCEP =  await fetch (`https://viacep.com.br/ws/${CEP}/json/`) // fatch recebe a URL da API // async ---> await
+      const consultaCEP =  await fetch (`https://viacep.com.br/ws/${cep}/json/`) // fatch recebe a URL da API // async ---> await
       const consultaCEPConvertida = await consultaCEP.json()
       if ( consultaCEP.erro){
         throw Error ('Esse CEP é inválido !')
       }
+      // iniciando a auto preechimento dos campos (cidade/ logradouro / estado) com base no Cep fornecido!
+      const cidade = document.getElementById('cidade');
+      const logradouro = document.getElementById('endereco');
+      const estado = document.getElementById('estado');
+      const bairro = document.getElementById('bairro');
+
+      cidade.value = consultaCEPConvertida.localidade
+      logradouro.value = consultaCEPConvertida.logradouro;
+      estado.value = consultaCEPConvertida.uf;
+      bairro.value = consultaCEPConvertida.bairro;
+      
+
       console.log(consultaCEPConvertida)
       return consultaCEPConvertida;
   } catch(erro){
+    mensagemErro.innerHTML =`<p> Esse CEP está incorreto. Tente novamente!</p>`
     console.log(erro)
   }
 }
+
+// -> buscando o cep e aplicando o valor no buscaEndereco 
+ const cep = document.getElementById('cep');
+ cep.addEventListener("focusout", () => buscarEndereco(cep.value));
 
